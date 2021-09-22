711211481b259564912032c5618782584343a094
pratik <pratiksherke97@gmail.com>
2021-09-22 18:13:13 +0530

    initial commit

---
+++ src/main/java/com/example/cachedemo/repository/ItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.cachedemo.repository;
+
+import com.example.cachedemo.model.Item;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ItemRepository extends CrudRepository<Item, String> {
+
+}
